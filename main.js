531d3c1b17026aab81b4e79c83c90bc39f0b9a5c
NordikE <erik.b10.kodehode@gmail.com>
2024-02-08 11:07:55 +0100

    .

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import people, { peopleArrayWithObjects } from "./datastruktur-metoder_og_JSON.js" assert { type: "json" };
+import { peopleArrayWithObjects } from "./datastruktur-metoder_og_JSON.js" assert { type: "json" };
 
 // 1.0
 
@@ -7,7 +7,7 @@ console.log(peopleArrayWithObjects);
 // 1.1
 
 const firstPerson = peopleArrayWithObjects[0];
-const lastPerson = peopleArrayWithObjects[4];
+const lastPerson = peopleArrayWithObjects[peopleArrayWithObjects.length -  1];
 
 console.log(firstPerson);
 console.log(lastPerson);
@@ -16,5 +16,5 @@ console.log(lastPerson);
 
 // 1.2
 
-let person = peopleArrayWithObjects.map;
+let person = Object.entries (peopleArrayWithObjects);
 
